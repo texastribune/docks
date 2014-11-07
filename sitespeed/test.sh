9bc5f96f825f26e77d6b875e3b558fd2e5f7ebaf
Daniel Craigmile <dcraigmile@texastribune.org>
2014-11-06 18:26:56 -0600

    test script

---
+++ sitespeed/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+phantomjs yslow.js -i grade -t '{"overall": "C", "ycdn": "F"}' -f tap http://test-subject:8000/ > /results/yslow.tap
+phantomjs yslow.js -i comps -f xml http://test-subject:8000/ > /results/yslow.xml
