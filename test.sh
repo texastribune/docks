9e933ff8132a98ba9b4418afcb0da4e44a9ce55a
Daniel Craigmile <dcraigmile@texastribune.org>
2014-11-06 19:59:23 -0600

    add yslow back

--- test.sh
+++ test.sh
@@ -8,9 +8,9 @@ set -o xtrace
 
 /etc/init.d/xvfb start
 
-#phantomjs yslow.js -i comps -f xml http://test-subject:8000/ > /results/yslow.xml
+phantomjs yslow.js -i comps -f xml http://test-subject:8000/ > /results/yslow.xml
 # this will exit with a non-zero code if the tests don't pass:
-#phantomjs yslow.js -i grade -t '{"overall": "C", "ycdn": "F"}' -f tap http://test-subject:8000/ > /results/yslow.tap
+phantomjs yslow.js -i grade -t '{"overall": "C", "ycdn": "F"}' -f tap http://test-subject:8000/ > /results/yslow.tap
 
 export DISPLAY=:1.0
 #sitespeed.io -r /results -d 0 -b firefox -u http://test-subject:8000/
