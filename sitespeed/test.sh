665c34e6af9f0f06938a95069114c283f1b2f5d8
Daniel Craigmile <dcraigmile@texastribune.org>
2014-11-06 18:47:15 -0600

    reorder tests

--- sitespeed/test.sh
+++ sitespeed/test.sh
@@ -7,8 +7,8 @@ set -o xtrace
 
 /etc/init.d/xvfb start
 
-phantomjs yslow.js -i grade -t '{"overall": "C", "ycdn": "F"}' -f tap http://test-subject:8000/ > /results/yslow.tap
 phantomjs yslow.js -i comps -f xml http://test-subject:8000/ > /results/yslow.xml
+phantomjs yslow.js -i grade -t '{"overall": "C", "ycdn": "F"}' -f tap http://test-subject:8000/ > /results/yslow.tap
 
 sitespeed.io -r /results -d 0 -b firefox -u http://test-subject:8000/
 sitespeed-junit.io -r /results -o /results -l 85 -a 85
