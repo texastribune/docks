4dcdd59d19c332a4dca2525dcf29d7cfe2d42317
Daniel Craigmile <dcraigmile@texastribune.org>
2014-11-06 18:46:07 -0600

    start frame buffer

--- sitespeed/test.sh
+++ sitespeed/test.sh
@@ -5,5 +5,7 @@ set -o errexit
 set -o pipefail
 set -o xtrace
 
+/etc/init.d/xvfb start
+
 phantomjs yslow.js -i grade -t '{"overall": "C", "ycdn": "F"}' -f tap http://test-subject:8000/ > /results/yslow.tap
 phantomjs yslow.js -i comps -f xml http://test-subject:8000/ > /results/yslow.xml
