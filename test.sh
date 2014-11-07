ef2526ff54f3b4570a6223e65ae6e3337063fa9b
Daniel Craigmile <dcraigmile@texastribune.org>
2014-11-06 18:50:37 -0600

    check exit status

--- test.sh
+++ test.sh
@@ -8,7 +8,11 @@ set -o xtrace
 /etc/init.d/xvfb start
 
 phantomjs yslow.js -i comps -f xml http://test-subject:8000/ > /results/yslow.xml
+echo $?
 phantomjs yslow.js -i grade -t '{"overall": "C", "ycdn": "F"}' -f tap http://test-subject:8000/ > /results/yslow.tap
+echo $?
 
 sitespeed.io -r /results -d 0 -b firefox -u http://test-subject:8000/
+echo $?
 sitespeed-junit.io -r /results -o /results -l 85 -a 85
+echo $?
