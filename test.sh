17861e78376eb6ef2caf00e736c77c0002e901a7
Daniel Craigmile <dcraigmile@texastribune.org>
2014-11-10 15:21:56 -0600

    sitespeed ignores multiple URLs

--- test.sh
+++ test.sh
@@ -8,11 +8,9 @@ set -o xtrace
 
 # gather CL args into list of URLs:
 URLS=""
-SITESPEED_URLS=""
 for var in "$@"
 do
   URLS="${URLS} http://test-subject:8000${var}"
-  SITESPEED_URLS="${SITESPEED_URLS} -u http://test-subject:8000${var}"
 done
 
 YSLOW_THRESHOLDS=${YSLOW_THRESHOLDS:-'{"overall": "0",
@@ -27,7 +25,8 @@ YSLOW_THRESHOLDS=${YSLOW_THRESHOLDS:-'{"overall": "0",
   "ycdn": "F" }'}
 
 # we get the overall score, the response time and load time from here:
-phantomjs yslow.js -i comps -f xml ${URLS} > /results/yslow.xml
+# we only look at "/" because that's the only page we grab timings from:
+phantomjs yslow.js -i comps -f xml http://test-subject:8000/ > /results/yslow.xml
 # this will exit with a non-zero code if the tests don't pass:
 for URL in "$@"
 do
@@ -40,7 +39,9 @@ export DISPLAY=:1.0
 
 # remove any previous results
 rm -r /results/test-subject
-sitespeed.io -r /results -d 0 -b firefox ${SITESPEED_URLS}
+# if you provide multiple --url looks like it ignores all but the last so doing
+# depth=1 instead:
+sitespeed.io -r /results -d 1 -b firefox --url http://test-subject:8000/
 mv /results/test-subject/*/* /results/test-subject/
 
 #sitespeed.io -d 0 --tap --url http://test-subject:8000 "-b chrome -n 1" > /results/sitespeed.tap
